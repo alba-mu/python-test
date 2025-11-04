1bb5727e9d52eebbc7c22a03835c2dd7caed0aec
alba-mu <albamu2002@gmail.com>
2025-11-04 17:54:44 +0100

    Separate testing functions for each case

--- test_division.py
+++ test_division.py
@@ -3,17 +3,21 @@ import pytest
 def division(a,b):
    return a / b
 
-def test_division():
+def test_int_division():
    assert division(6,3) == 2
    assert division(9,3) == 3
-   assert division(5, 2) == 2.5
-   #assert division('6', 3)
-   #assert division(6, '3')
-   #assert division(4, 0)
-   assert division(2, 3)
-   #assert division('a', 3)
-   #assert division(3, 'a')
 
+def test_float_division():
+   assert division(5, 2) == 2.5
+   assert division(2, 3)
+
+def test_with_alphabetic_strings():
+    with pytest.raises(TypeError):
+        division('a', 3)
+
+def test_with_numeric_strings():
+    with pytest.raises(TypeError):
+        division('6', 3)
 
 def test_zero_division():
     with pytest.raises(ZeroDivisionError):
