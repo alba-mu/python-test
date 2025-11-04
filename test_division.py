32987108cae96889b66dec64b4449742e89d1004
alba-mu <albamu2002@gmail.com>
2025-11-04 16:54:54 +0100

    List of all the test cases that should be tested for the division operator (/)

--- test_division.py
+++ test_division.py
@@ -4,3 +4,8 @@ def division(a,b):
 def test_division():
    assert division(6,3) == 2
    assert division(9,3) == 3
+   assert division(5, 2) == 2.5
+   assert division(4, 0)
+   assert division(2, 3)
+   assert division('a', 3)
+   assert division(3, 'a')
